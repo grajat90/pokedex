2ba23bca871531fe6aaea3969faabeadedfe629f
Rajat <rajat@capitsign.com>
2024-01-16 22:59:16 +0530

    Fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import {useState} from "react";
-import {Tab, Tabs} from "@mui/material";
+import {Paper, Tab, Tabs} from "@mui/material";
 import SearchByName from "~/_blocks/SearchByName";
 import SearchMany from "~/_blocks/SearchMany";
 import FilterablePokedexTable from "~/_components/FilterablePokedexTable";
@@ -14,14 +14,14 @@ export default function Home() {
                 <title>Pokédex</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main className="md:px-20 py-10 flex h-screen min-w-screen flex-col items-center justify-items-start bg-slate-50 gap-10">
+            <main className="md:px-20 px-5 py-10 flex h-screen min-w-screen flex-col items-center justify-items-start bg-slate-50 gap-10">
                 <div>
                     <span className="font-mono font-bold text-7xl text-amber-400 font-outline-4">Pokédex</span>
                 </div>
-                <Tabs value={selectedTab} onChange={(event, value: number) => {
+                <Tabs variant="scrollable" className="rounded-md" component={Paper} value={selectedTab} onChange={(event, value: number) => {
                     setSelectedTab(value)
                 }}>
-                    <Tab label="Search by name"/>
+                    <Tab label="Search one"/>
                     <Tab label="Search many"/>
                     <Tab label="Search by type"/>
                 </Tabs>
